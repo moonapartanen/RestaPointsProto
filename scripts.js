f61f3fb3de38dd1495ceae7439f4b1e7e853289e
Moona E Partanen <Moona.Partanen@edu.savonia.fi>
2018-04-12 10:58:26 +0300

    Map markers updated

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ var markerMountSherpa;
 
 $(document).ready(function(){
 
+	InitializeMap()
 
-	Roulette();
+	markerDragonSheng.addListener('click', function() {
+		$("#modalDragonSheng").modal();
+	});
 
+	markerMountSherpa.addListener('click', function() {
+		$("#modalMountSherpa").modal();
+	});
 });
 	
 function InitializeMap()
@@ -16,16 +22,42 @@ function InitializeMap()
 	});
 	
 	markerDragonSheng = new google.maps.Marker({
-          position: new google.maps.LatLng(62.8946901, 27.676138499),
-          map: map,
-          title: 'Dragon Sheng'
-		});
-	
-	var markerHarald = new google.maps.Marker({
-		position: new google.maps.LatLng(62.8925755, 27.676264700),
+		position: new google.maps.LatLng(62.8946901, 27.676138499),
 		map: map,
-		title: 'Viikinkiravintola Harald'
-		});
+		title: 'Dragon Sheng',
+		label: {
+			text: "Dragon Sheng: 520 pistettä",
+			color: "black",
+			fontSize: "14px",
+			fontWeight: "bold"
+		  }
+	  });
+  
+  var markerHarald = new google.maps.Marker({
+	  position: new google.maps.LatLng(62.8925755, 27.676264700),
+	  map: map,
+	  title: 'Viikinkiravintola Harald',
+	  label: {
+		text: "Harald: 300 pistettä",
+		color: "black",
+		fontSize: "14px",
+		fontWeight: "bold"
+	  	}
+	  });
+
+  markerMountSherpa = new google.maps.Marker({
+	  position: new google.maps.LatLng(62.8928034, 27.68115119),
+	  map: map,
+	  title: 'Mount Sherpa',
+	  label: {
+		text: "Mount Sherpa: 730 pistettä",
+		color: "black",
+		fontSize: "14px",
+		fontWeight: "bold"
+	  	}
+	  });
+}
+
 
 
 
